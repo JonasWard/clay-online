6ec99f2503daba387d0cf161bf073e9a3ac90794
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2021-11-01 18:29:08 +0100

    three translation apps

---
+++ src/geometry/to-three.js
@@ -0,0 +1,6 @@
+import {Vector3, Curve, Mesh, TubeGeometry} from "three";
+
+export function TubeGeo(path, segs, radius, radSegs, closed, material) {
+    const geo = new TubeGeometry(path, segs, radius, radSegs, closed);
+    return new Mesh(geo, material);
+}
