fe5e06788a575856793d98dcaf7e4dff2d4861e4
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2021-11-03 18:04:47 +0100

    full screen

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import ReactDOM from "react-dom";
 import * as THREE from "three";
 import {TestGeo} from "./geometry/test-geo";
 import {setUp} from "./three-setup/set-up";
+
+import './App.css';
 import Viewer from "./Viewer";
 
 function App() {
     return (
-        <div className={'App'}>
-            <Viewer/>
+        <div className={'app'}>
+            <div className={'container'}>
+                <Viewer/>
+            </div>
         </div>
     );
 }
