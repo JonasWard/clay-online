0dc7a5873057b3f6a27d1c534d22a6cc981d7bf5
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2021-11-05 21:34:09 +0100

    base clay point class

---
+++ src/geometry/clayBrick/clay-point.js
@@ -0,0 +1,47 @@
+import {Vector3} from "three";
+import {Coordinate} from "jsts/org/locationtech/jts/geom";
+
+export class ClayPoint {
+    origin;
+    direction;
+    position;
+
+    constructor(origin, direction) {
+        this.origin = origin;
+        this.position = new Vector3().copy(origin);
+        this.direction = direction;
+    }
+
+    move(value) {
+        this.position = new Vector3().addVectors(
+            this.position,
+            new Vector3().addScaledVector(this.direction, value)
+        );
+    }
+
+    set(value) {
+        this.position = new Vector3().addVectors(
+            this.origin,
+            new Vector3().addScaledVector(this.direction, value)
+        );
+    }
+
+    toCoordinate(value) {
+        return new Coordinate(
+            this.position.x,
+            this.position.y,
+            this.position.z
+        )
+    }
+}
+
+export function clayPointTest() {
+    const vBase = new Vector3(0, 0, 0);
+    const vDir = new Vector3(1, 0, 0);
+
+    const clayPoint = new ClayPoint(vBase, vDir);
+
+    clayPoint.move(1.);
+
+    clayPoint.set(10.);
+}
