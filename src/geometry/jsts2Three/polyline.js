1f08cf14b9cdbe5ca10a45e2ba0cf1fd89c09797
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2021-11-06 12:39:58 +0100

    fixed tangents copy bug

--- src/geometry/jsts2Three/polyline.js
+++ src/geometry/jsts2Three/polyline.js
@@ -1,4 +1,5 @@
-import {Curve, Vector3} from 'three';
+import {Curve, Matrix4, Vector3} from 'three';
+import {clamp} from "three/src/math/MathUtils";
 
 export class Polyline extends Curve {
     constructor(points, closed = true) {
@@ -111,6 +112,126 @@ export class Polyline extends Curve {
 
         vT.normalize();
         return optionalTarget.set(vT.x, vT.y, vT.z);
+    }
+
+    computeFrenetFrames( segments, closed ) {
+
+        // see http://www.cs.indiana.edu/pub/techreports/TR425.pdf
+
+        const normal = new Vector3();
+
+        const tangents = [];
+        const normals = [];
+        const binormals = [];
+
+        const vec = new Vector3();
+        const mat = new Matrix4();
+
+        // compute the tangent vectors for each segment on the curve
+
+        for ( let i = 0; i <= segments; i ++ ) {
+
+            const u = i / segments;
+
+            tangents[ i ] = this.getTangentAt( u, new Vector3() );
+
+        }
+
+        // select an initial normal vector perpendicular to the first tangent vector,
+        // and in the direction of the minimum tangent xyz component
+
+        normals[ 0 ] = new Vector3();
+        binormals[ 0 ] = new Vector3();
+        let min = Number.MAX_VALUE;
+        const tx = Math.abs( tangents[ 0 ].x );
+        const ty = Math.abs( tangents[ 0 ].y );
+        const tz = Math.abs( tangents[ 0 ].z );
+
+        if ( tx <= min ) {
+
+            min = tx;
+            normal.set( 1, 0, 0 );
+
+        }
+
+        if ( ty <= min ) {
+
+            min = ty;
+            normal.set( 0, 1, 0 );
+
+        }
+
+        if ( tz <= min ) {
+
+            normal.set( 0, 0, 1 );
+
+        }
+
+        vec.crossVectors( tangents[ 0 ], normal ).normalize();
+
+        normals[ 0 ].crossVectors( tangents[ 0 ], vec );
+        binormals[ 0 ].crossVectors( tangents[ 0 ], normals[ 0 ] );
+
+
+        // compute the slowly-varying normal and binormal vectors for each segment on the curve
+
+        for ( let i = 1; i <= segments; i ++ ) {
+
+            normals[ i ] = normals[ i - 1 ].clone();
+
+            binormals[ i ] = binormals[ i - 1 ].clone();
+
+            vec.crossVectors( tangents[ i - 1 ], tangents[ i ] );
+
+            if ( vec.length() > Number.EPSILON ) {
+
+                vec.normalize();
+
+                const theta = Math.acos( clamp( tangents[ i - 1 ].dot( tangents[ i ] ), - 1, 1 ) ); // clamp for floating pt errors
+
+                normals[ i ].applyMatrix4( mat.makeRotationAxis( vec, theta ) );
+
+            }
+
+            binormals[ i ].crossVectors( tangents[ i ], normals[ i ] );
+
+        }
+
+        // if the curve is closed, postprocess the vectors so the first and last normal vectors are the same
+
+        if ( closed === true ) {
+
+            let theta = Math.acos( clamp( normals[ 0 ].dot( normals[ segments ] ), - 1, 1 ) );
+            theta /= segments;
+
+            if ( tangents[ 0 ].dot( vec.crossVectors( normals[ 0 ], normals[ segments ] ) ) > 0 ) {
+
+                theta = - theta;
+
+            }
+
+            for ( let i = 1; i <= segments; i ++ ) {
+
+                // twist a little...
+                normals[ i ].applyMatrix4( mat.makeRotationAxis( tangents[ i ], theta * i ) );
+                binormals[ i ].crossVectors( tangents[ i ], normals[ i ] );
+
+            }
+
+        }
+
+        console.log({
+            tangents: tangents,
+            normals: normals,
+            binormals: binormals
+        });
+
+        return {
+            tangents: tangents,
+            normals: normals,
+            binormals: binormals
+        };
+
     }
 
     _tConstraining(t) {
