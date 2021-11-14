dd50d071a34892224ad14a7e6b8604944493c428
Jonas Ward Van den Bulcke <43600893+JonasWard@users.noreply.github.com>
2021-11-14 16:24:07 +0100

    first pattern functions

---
+++ src/geometry/clayBrick/clay-patterns.js
@@ -0,0 +1,12 @@
+export function sinWaveUVPattern(uv, parameters){
+    const phase = parameters.phaseDelta * uv.y;
+    return Math.sin(uv.x * parameters.frequency + phase) * parameters.amplitude + parameters.offset;
+}
+
+export const DEFAULT_SIN_WAVE_UV_PARAMETERS = {
+    amplitude: 2.,
+    frequency: 2. * Math.PI / 30.0,
+    phaseDelta: 0.,
+    offset: 5.,
+    uv: true
+};
