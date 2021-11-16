61e2dba1030fcca1e645026cf4e48e2083050e7c
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2021-11-17 00:26:44 +0100

    initializing all pattern & general geometry variables

--- src/three-setup/gui-setup.js
+++ src/three-setup/gui-setup.js
@@ -1,33 +1,53 @@
 import {GUI} from "three/examples/jsm/libs/dat.gui.module";
 import {USDZExporter} from "three/examples/jsm/exporters/USDZExporter";
+import {addBrick, clearScene, overwrites} from "../geometry/three/brick-to-scene";
+import {PATTERN_LIST} from "../geometry/clayBrick/clay-patterns";
 
 export function createGUI(scene) {
-    const gui = new GUI();
+    const gui = new GUI({ autoPlace: true, width: 150 });
 
-    const exportUSDZ = { usdz:function(){
-        console.log("export USDZ !?");
+    for (const patternName in PATTERN_LIST) {
+        const localPatternGUI = gui.addFolder(patternName);
 
-        // const exporter = new USDZExporter();
-        // const arrayBuffer = exporter.parse( scene, function ( ucsd ) {
-        //     console.log(ucsd);
-        //     console.log("nothing ?");
-        //
-        // }, {} );
-        //
-        // const blob = new Blob( [ arrayBuffer ], { type: 'application/octet-stream' } );
-        //
-        // // console.log(ucsd);
-        // console.log(document);
-        //
-        // const link = document.getElementById( 'link' );
-        //
-        // console.log(link);
-        //
-        // link.href = URL.createObjectURL( blob );
+        console.log(PATTERN_LIST);
 
-    }};
+        for (const patternParameter in PATTERN_LIST[patternName].patternParameters) {
+            addPatternOverwrites(localPatternGUI, patternParameter, patternName);
+        }
+    }
 
-    gui.add(exportUSDZ,'usdz');
+    const defaultParameters = gui.addFolder("constructionParameters");
 
-    return gui;
+    for (const overwrite in overwrites) {
+        if (overwrite !== "pattern") {
+            addAllOverwrites(defaultParameters, overwrite);
+        }
+    }
+}
+
+function addAllOverwrites(guiElement, overwriteElement) {
+    guiElement.add(
+        overwrites,
+        overwriteElement,
+        overwrites[overwriteElement].min,
+        overwrites[overwriteElement].max
+    ).onChange(function (value) {
+        overwrites[overwriteElement] = value;
+        addBrick();
+    });
+}
+
+function addPatternOverwrites(guiElement, overwritePatternElement, patternName) {
+    overwrites.pattern.patternFunction = PATTERN_LIST[patternName].patternFunction;
+
+    guiElement.add(
+
+        overwrites.pattern.patternParameters,
+        overwritePatternElement,
+        overwrites.pattern.patternParameters[overwritePatternElement].min,
+        overwrites.pattern.patternParameters[overwritePatternElement].max
+    ).onChange(function (value) {
+        overwrites.pattern.patternParameters[overwritePatternElement] = value;
+        addBrick();
+    });
 }
