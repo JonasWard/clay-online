31986b8dd76bb3f98f10112c4985d53d4121551b
Jonas Ward Van den Bulcke <jonas.vandenbulcke@gmail.com>
2021-11-17 00:25:37 +0100

    updated overwrite settings added textured mesh

--- src/geometry/three/brick-to-scene.js
+++ src/geometry/three/brick-to-scene.js
@@ -2,30 +2,66 @@ import {constructBrick} from "../clayBrick/clay-simple-base";
 import {TubeGeo} from "../to-three";
 import {shaderNormal} from "../test-geo";
 import {DEFAULT_SIN_WAVE_UV_PARAMETERS, sinWaveUVPattern} from "../clayBrick/clay-patterns";
+import {MeshLambertMaterial, TextureLoader, TubeGeometry, Mesh, DirectionalLight} from "three";
+import {scene, renderer} from "../../three-setup/set-up";
 
-export let overwrites = {
-    productionWidth: 2.5,
-    baseWidth: 150.0,
-    baseLength: 300.0, // always needs to be at least the same length as the brick's width
-    lengthBufferMultiplier: 2.0,
-    pinSpacing: 220.0,
-    diamondWidth: 50.0,
-    diamondHeight: 110.0,
-    diamondCount: 3,
-    pinDiameter0: 40.0,
-    pinDiameter1: 20.0,
-    pinDelta: 250.0,
-    pinDivisions: 60,
-    precision: 2.0,
-    bodyHeight: 160.0,
-    totalHeight: 260.0,
-    startHeight: -100.0,
-    layerHeight: 2.5,
+const CLAY_TEXTURE_URL = "https://i.ibb.co/9nk0c8H/terra-cotta-stucco-wall-texture.jpg";
+
+export let geometryArray = [];
+
+export const OVERWRITE_SETTINGS = {
+    productionWidth: {default: 2.5, min: 1., max: 10.},
+    baseWidth: {default: 150.0, min: 100., max: 200.},
+    baseLength: {default: 300.0, min: 200., max: 400.},
+    lengthBufferMultiplier: {default: 2.0, min: 1.0, max: 10.},
+    pinSpacing: {default: 220.0, min: 100., max: 300.},
+    diamondWidth: {default: 50.0, min: 20., max: 100.},
+    diamondHeight: {default: 110.0, min: 20., max: 200.},
+    diamondCount: {default: 3, min: 1, max: 5},
+    pinDiameter0: {default: 40.0, min: 30., max: 100.},
+    pinDiameter1: {default: 20.0, min: 30., max: 100.},
+    pinDelta: {default: 250.0, min: 1.0, max: 10.},
+    pinDivisions: {default: 60, min: 1.0, max: 10.},
+    precision: {default: 2.0, min: 1.0, max: 10.},
+    bodyHeight: {default: 160.0, min: 1.0, max: 10.},
+    totalHeight: {default: 260.0, min: 1.0, max: 10.},
+    startHeight: {default: -100.0, min: 1.0, max: 10.},
+    layerHeight: {default: 2.5, min: 1.0, max: 10.},
     pattern: {
-        patternFunction: sinWaveUVPattern,
-        patternParameters: DEFAULT_SIN_WAVE_UV_PARAMETERS
+        patternFunction: {default: sinWaveUVPattern},
+        patternParameters: {default: DEFAULT_SIN_WAVE_UV_PARAMETERS}
     }
 }
+
+function overwriteClone() {
+    let overwriteCopy = {};
+
+    for (const key in OVERWRITE_SETTINGS) {
+        if (key !== "pattern") {
+            overwriteCopy[key] = OVERWRITE_SETTINGS[key].default;
+        } else {
+            overwriteCopy["pattern"] = {
+                patternFunction: OVERWRITE_SETTINGS.pattern.patternFunction.default,
+                patternParameters: patternClone()
+            };
+        }
+    }
+
+    return overwriteCopy;
+}
+
+function patternClone() {
+    let patternSettingsClone = {};
+
+    for (const key in OVERWRITE_SETTINGS.pattern.patternParameters.default) {
+        patternSettingsClone[key] = OVERWRITE_SETTINGS.pattern.patternParameters.default[key].default;
+    }
+
+    return patternSettingsClone;
+}
+
+export let overwrites = overwriteClone();
+
 
 export function patternMapping(patternName) {
     switch (patternName) {
@@ -36,11 +72,66 @@ export function patternMapping(patternName) {
     }
 }
 
-export function addBrick(scene) {
-    scene.children = [];
+function applyBrickShader(scene, pls) {
+    const loader = new TextureLoader();
 
-    for (const pl of constructBrick(overwrites)) {
-        const locTubeGeo = TubeGeo(pl, pl.getPointCount(), 1.5, 6, false, shaderNormal());
-        scene.add(locTubeGeo);
+
+    loader.load(
+        // resource URL
+        CLAY_TEXTURE_URL,
+
+        // onLoad callback
+        function ( texture ) {
+            // in this example we create the material when the texture is loaded
+            const brickShader = new MeshLambertMaterial( {
+                map: texture
+            } );
+
+            // const brickShader = shaderNormal();
+
+            for (const pl of pls) {
+                const tubeGeo = new TubeGeometry(pl, pl.getPointCount(), 1.5, 6, false);
+                const locMesh = new Mesh(tubeGeo, brickShader);
+
+                geometryArray.push(locMesh);
+
+                scene.add(locMesh);
+            }
+        },
+
+        // onProgress callback currently not supported
+        undefined,
+
+        // onError callback
+        function ( err ) {
+            console.error( 'An error happened.' );
+        }
+    );
+}
+
+export function addLighting(scene) {
+    const directionalLight = new DirectionalLight( 0xffffff, 1.1 );
+    directionalLight.position.set(500,500,500);
+    scene.add( directionalLight );
+}
+
+export function clearScene(scene){
+    for (const obj of geometryArray) {
+        scene.remove(obj);
+        obj.geometry.dispose();
+        obj.material.dispose();
     }
+
+    geometryArray = [];
+}
+
+export function addBrick() {
+
+    clearScene(scene);
+
+    const pls = constructBrick(overwrites)
+
+    applyBrickShader(scene, pls);
+
+    console.log(scene.children);
 }
